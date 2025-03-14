6a1a3e0af69c951c5d6cf4ae94384ab260f783b4
Matrixioo <105792451+Matrixioo@users.noreply.github.com>
2025-03-14 17:01:31 +0100

    Add files via upload

---
+++ telegram-bots/ai_implementation-bot/main.py
@@ -0,0 +1,54 @@
+import requests
+import asyncio
+from aiogram import Dispatcher, types, Bot
+from aiogram.types import Message
+import logging
+
+TOKEN = "TOKEN" #token here (obviously i won't put any of mines)
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+url = "https://api.intelligence.io.solutions/api/v1/chat/completions"
+
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer TOKEN_HERE"
+}
+
+@dp.message()
+async def response_from_ai(message: Message):
+
+    user_input = message.text
+
+    data = {
+        "model": "deepseek-ai/DeepSeek-R1",
+        "messages": [
+            {
+                "role": "system",
+                "content": "you are a chat bot in telegram named @morgena_bot, so you are morgena. you are male"
+            },
+            {
+                "role": "user",
+                "content": user_input
+            }
+        ]
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+    fresponse = response.json()
+
+    text = fresponse['choices'][0]['message']['content']
+    ftext = text.split("</think>\n\n")[1]
+
+    print(ftext)
+    await message.answer(ftext)
+
+
+
+async def main():
+    logging.basicConfig(level = logging.INFO)
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
